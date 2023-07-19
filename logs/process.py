44743ecaa020521b1a566693538db6da941c7b34
MarcusWheeler <wheemarc@oregonstate.edu>
2023-07-19 13:35:04 -0700

    Fixed minor bug I introduced in last push

--- logs/process.py
+++ logs/process.py
@@ -1,17 +1,9 @@
 import matplotlib.pyplot as plt
 class Processor:
     def __init__(self, logs_path):
-    
-        self.list_rewards = []
-        self.list_ac = []
-        self.list_ev = []
-        self.list_sh = []
-        self.list_enc = []
-        self.list_att = []
-        self.list_attsp = []
         self.logs_path = logs_path
-        self.list_dict = {0:self.list_rewards, 1:self.list_ac, 2:self.list_ev, 3:self.list_sh, 4:self.list_enc, 5:self.list_att, 6:self.list_attsp}
-        self.slot_dict = {0:"Rewards", 1:"AC", 2:"EV", 3:"SH", 4:"ENC", 5:"ATT", 6:"ATTSP"}
+        self.list_dict = {0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[]}
+        self.slot_dict = {0:"Reward", 1:"AC", 2:"EV", 3:"SH", 4:"ENC", 5:"ATT", 6:"ATTSP"}
     
     def set_up_lists(self):
         with open(self.logs_path + 'terminated_logs.txt','r') as f:
@@ -24,8 +16,9 @@ class Processor:
                     concatenated = "Final " + self.slot_dict[key]
                     #If you're in the entry, use the key on the list_dict to get the proper list
                     if concatenated in entry:
-                        self.list_dict[key].append(float(entry.split(":")[1].split(" ")[2].split("\n")[0]))
-
+                        print(entry.split(":")[1].split(" ")[2].split("\n")[0])
+                        if key == 0:
+                            self.list_dict[key].append(float(entry.split(":")[1].split(" ")[2].split("\n")[0]))
 
     def plot_list(self,fig, ax, data, data_string, save_string):
         # Plot some data
